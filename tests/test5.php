9a8b04acb8d331a2d637d356c2bef47383edaa67
Derick Rethans <derick@derickrethans.nl>
2002-05-12 10:57:46 +0000

    - Added test 5 for testing nested function calls - Added test 6 for multi-dimensional constant arrays passed to functions.
    SVN Rev: 80

---
+++ tests/test5.php
@@ -0,0 +1,33 @@
+<?php
+
+function fucking() {
+    deep();
+}
+
+function deep() {
+    nested();
+}
+
+function nested() {
+    error();
+}
+
+function error() {
+    in();
+}
+
+function in() {
+    func();
+}
+
+function func() {
+    blah();
+}
+
+function blah() {
+    funky_shit();
+}
+
+fucking();
+
+?>
