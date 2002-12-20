e0a77cef3deebe2f698216721001e865f5a5bac5
Derick Rethans <derick@derickrethans.nl>
2002-12-20 15:47:17 +0000

    - Make this better working
    SVN Rev: 528

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-TEST_PHP_EXECUTABLE=../php-4.4.0dev/sapi/cli/php php ../php-4.4.0dev/run-tests.php tests/*.phpt
+TEST_PHP_EXECUTABLE=/usr/local/bin/php /usr/local/bin/php /dat/dev/php/php-4.4.0dev/run-tests.php tests/*.phpt
