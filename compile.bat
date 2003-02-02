3402a7992335527b293f5ade7732b71d86299662
Derick Rethans <derick@derickrethans.nl>
2003-02-02 18:52:14 +0000

    - Fix some typoes
    SVN Rev: 591

--- compile.bat
+++ compile.bat
@@ -1,5 +1,7 @@
+set HOMEDRIVE=C:
+set HOMEPATH=\
 d:
-cd php\xdebug
+cd \php\xdebug
 d:\cvs upd
 nmake /f xdebug.mak "CFG=xdebug - Win32 Release_TS" clean all
-cp .\Release_TS\php_xdebug.dll h:\
+copy Release_TS\php_xdebug.dll h:\
