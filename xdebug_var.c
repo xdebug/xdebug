03f836feda0539160f6764efba88bdadb550a587
Derick Rethans <derick@derickrethans.nl>
2003-02-27 15:02:08 +0000

    - Improve memory allocation code by a factor of about 500
    SVN Rev: 604

--- xdebug_var.c
+++ xdebug_var.c
@@ -38,7 +38,8 @@ void XDEBUG_STR_ADD(xdebug_str *xs, char *str, int f)
 	if (!xs->l) {
 		xs->d[0] = '\0';
 	}
-	strcat (xs->d, str);
+	memcpy(xs->d + xs->l, str, l);
+	xs->d[xs->l + l] = '\0';
 	xs->l = xs->l + l;
 	if (f) {
 		xdfree(str);
@@ -54,8 +55,10 @@ void XDEBUG_STR_ADDL(xdebug_str *xs, char *str, int le, int f)
 	if (!xs->l) {
 		xs->d[0] = '\0';
 	}
-	strcat (xs->d, str);
+	memcpy(xs->d + xs->l, str, le);
+	xs->d[xs->l + le] = '\0';
 	xs->l = xs->l + le;
+
 	if (f) {
 		xdfree(str);
 	}
