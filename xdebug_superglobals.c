245fc42e23ba89611baf65b9c871b553757188f9
Derick Rethans <derick@derickrethans.nl>
2003-04-04 22:54:50 +0000

    - Header maintenance
    SVN Rev: 612

--- xdebug_superglobals.c
+++ xdebug_superglobals.c
@@ -12,7 +12,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors:  Harald Radi <harald.radi@nme.at                            |
+   | Authors: Harald Radi <harald.radi@nme.at>                            |
    +----------------------------------------------------------------------+
  */
 
