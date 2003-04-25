e4ba7b54047565c6700765bced3b1a30b419bbbb
Derick Rethans <derick@derickrethans.nl>
2003-04-25 15:59:28 +0000

    - Also automatically build the 4.3 version
    SVN Rev: 689

--- compile.bat
+++ compile.bat
@@ -3,5 +3,10 @@ set HOMEPATH=\
 d:
 cd \php\xdebug
 d:\cvs upd
+
+nmake /f xdebug_4_3.mak "CFG=xdebug - Win32 Release_TS" clean all
+copy Release_TS\php_xdebug.dll h:\php_xdebug_4_3.dll
+
 nmake /f xdebug.mak "CFG=xdebug - Win32 Release_TS" clean all
 copy Release_TS\php_xdebug.dll h:\
+
