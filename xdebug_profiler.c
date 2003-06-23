b66d5be56bd9b354036b730a0f0163d70eba96cf
Derick Rethans <derick@derickrethans.nl>
2003-06-23 21:55:51 +0000

    - Licence update. This change only renames "PHP Licence" to "Xdebug Licence",   "The PHP Group" to "Derick Rethans" and "PHP" to "Xdebug".
    SVN Rev: 718

--- xdebug_profiler.c
+++ xdebug_profiler.c
@@ -1,16 +1,16 @@
 /*
    +----------------------------------------------------------------------+
-   | PHP Version 4                                                        |
+   | Xdebug                                                               |
    +----------------------------------------------------------------------+
-   | Copyright (c) 2002, 2003 The PHP Group                               |
+   | Copyright (c) 2002, 2003 Derick Rethans                              |
    +----------------------------------------------------------------------+
-   | This source file is subject to version 2.02 of the PHP license,      |
+   | This source file is subject to version 1.0 of the Xdebug license,    |
    | that is bundled with this package in the file LICENSE, and is        |
    | available at through the world-wide-web at                           |
-   | http://www.php.net/license/2_02.txt.                                 |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
+   | http://xdebug.derickrethans.nl/license.php                           |
+   | If you did not receive a copy of the Xdebug license and are unable   |
+   | to obtain it through the world-wide-web, please send a note to       |
+   | xdebug@derickrethans.nl so we can mail you a copy immediately.       |
    +----------------------------------------------------------------------+
    | Author:   Ilia Alshanetsky <ilia@prohost.org>                        |
    +----------------------------------------------------------------------+
