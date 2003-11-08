e1f65e6d5ba15222d835b17dec44bd20098b0c4a
Derick Rethans <derick@derickrethans.nl>
2003-11-08 22:34:25 +0000

    - Update compile script with -dP
    SVN Rev: 946

--- compile.bat
+++ compile.bat
@@ -2,7 +2,7 @@ set HOMEDRIVE=C:
 set HOMEPATH=\
 d:
 cd \php\xdebug
-d:\cvs upd
+d:\cvs upd -dP
 
 nmake /f xdebug_4_3.mak "CFG=xdebug - Win32 Release_TS" clean all
 copy Release_TS\php_xdebug.dll h:\php_xdebug_4_3.dll
