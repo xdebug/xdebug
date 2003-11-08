c970c035fb09f6d81b014a6d65e1ceba6a81f896
Derick Rethans <derick@derickrethans.nl>
2003-11-08 23:02:47 +0000

    - Name .dlls more consistently
    SVN Rev: 948

--- compile.bat
+++ compile.bat
@@ -5,8 +5,8 @@ cd \php\xdebug
 d:\cvs upd -dP
 
 nmake /f xdebug_4_3.mak "CFG=xdebug - Win32 Release_TS" clean all
-copy Release_TS\php_xdebug.dll h:\php_xdebug_4_3.dll
+copy Release_TS\php_xdebug.dll h:\xdebug-4.3-1.3dev.dll
 
 nmake /f xdebug.mak "CFG=xdebug - Win32 Release_TS" clean all
-copy Release_TS\php_xdebug.dll h:\
+copy Release_TS\php_xdebug.dll h:\xdebug-5.0-1.3dev.dll
 
