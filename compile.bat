597c2f4bf3dfe6b941db214a69b5fe43cbc5c2a6
Derick Rethans <derick@derickrethans.nl>
2003-11-25 16:27:51 +0000

    - This pause should not be in
    SVN Rev: 962

--- compile.bat
+++ compile.bat
@@ -9,5 +9,3 @@ copy Release_TS\php_xdebug.dll h:\xdebug-4.3-2.0dev.dll
 
 nmake /f xdebug.mak "CFG=xdebug - Win32 Release_TS" clean all
 copy Release_TS\php_xdebug.dll h:\xdebug-5.0-2.0dev.dll
-
-pause
