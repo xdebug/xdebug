4ccfbc2e31e62cc1ca85bc147ccebbdd9873a57b
Derick Rethans <derick@derickrethans.nl>
2003-12-26 21:59:43 +0000

    - Update for Windows / Xdebug 1.3.0
    SVN Rev: 1037

--- compile.bat
+++ compile.bat
@@ -5,8 +5,9 @@ cd \php\xdebug
 d:\cvs upd -dP
 
 nmake /f xdebug_4_3.mak "CFG=xdebug - Win32 Release_TS" clean all
-copy Release_TS\php_xdebug.dll h:\xdebug-4.3-1.3dev.dll
+copy Release_TS\php_xdebug.dll c:\xdebug-4.3-1.3.0.dll
+copy Release_TS\php_xdebug.dll h:\xdebug-4.3-1.3.0.dll
 
 nmake /f xdebug.mak "CFG=xdebug - Win32 Release_TS" clean all
-copy Release_TS\php_xdebug.dll h:\xdebug-5.0-1.3dev.dll
-
+copy Release_TS\php_xdebug.dll h:\xdebug-5.0-1.3.0.dll
+copy Release_TS\php_xdebug.dll c:\xdebug-5.0-1.3.0.dll
