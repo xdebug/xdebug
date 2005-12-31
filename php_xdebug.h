8e44423ffc6761b06ff82728a76fa6cf3c1f4fe3
Derick Rethans <derick@derickrethans.nl>
2005-12-31 10:54:11 +0000

    - Go with beta5.
    SVN Rev: 1795

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,7 +20,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "2.0.0beta5-dev"
+#define XDEBUG_VERSION    "2.0.0beta5"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2005 by Derick Rethans"
 #define XDEBUG_URL        "http://xdebug.org"
