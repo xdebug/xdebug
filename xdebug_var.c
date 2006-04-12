6bbe277141d786a065a1c07898dcdd77125d3800
Derick Rethans <derick@derickrethans.nl>
2006-04-12 22:26:33 +0000

    - Fixed Komodo bug 45484: no member data for objects in PHP debugger.
    SVN Rev: 1902

--- xdebug_var.c
+++ xdebug_var.c
@@ -574,6 +574,15 @@ void xdebug_var_export_xml_node(zval **struc, char *name, xdebug_xml_node *node,
 					xdebug_xml_add_attribute_ex(node, "numchildren", xdebug_sprintf("%d", zend_hash_num_elements(myht)), 0, 1);
 					if (level <= options->max_depth) {
 						options->runtime[level].current_element_nr = 0;
+						if (level == 0 && myht->nNumOfElements > options->max_children) {
+							xdebug_xml_add_attribute_ex(node, "page", xdebug_sprintf("%d", options->runtime[level].page), 0, 1);
+							xdebug_xml_add_attribute_ex(node, "pagesize", xdebug_sprintf("%d", options->max_children), 0, 1);
+							options->runtime[level].start_element_nr = options->max_children * options->runtime[level].page;
+							options->runtime[level].end_element_nr = options->max_children * (options->runtime[level].page + 1);
+						} else {
+							options->runtime[level].start_element_nr = 0;
+							options->runtime[level].end_element_nr = options->max_children;
+						}
 						zend_hash_apply_with_arguments(myht, (apply_func_args_t) xdebug_object_element_export_xml_node, 4, level, node, name, options);
 					}
 				} else {
