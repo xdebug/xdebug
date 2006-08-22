09a51eb35f00b5109ae4072317399c5db207e7d7
Shane Caraveo <shane@caraveo.com>
2006-08-22 18:43:09 +0000

    fix xdebug builds against 5.0.x
    SVN Rev: 1991

--- xdebug.c
+++ xdebug.c
@@ -1917,7 +1917,11 @@ void xdebug_error_cb(int type, const char *error_filename, const uint error_line
 				 * but DO NOT overwrite a pending exception
 				 */
 				if (PG(error_handling) == EH_THROW && !EG(exception)) {
+#if PHP_MINOR_VERSION >= 1
 					zend_throw_error_exception(PG(exception_class), buffer, 0, type TSRMLS_CC);
+#else
+					zend_throw_exception(PG(exception_class), buffer, 0 TSRMLS_CC);
+#endif
 				}
 				efree(buffer);
 				return;
