a3d495d8857c945c35ea6c0857637e1035e89ab6
Derick Rethans <derick@derickrethans.nl>
2006-12-07 00:21:26 +0000

    - Fixed some issues with finding out the classname, which is important for   fetching private properties.
    SVN Rev: 2191

--- xdebug_handler_dbgp.c
+++ xdebug_handler_dbgp.c
@@ -458,10 +458,11 @@ static zval* get_symbol_contents_zval(char* name, int name_length TSRMLS_DC)
 							keyword_end = *p;
 							if (keyword) {
 								retval = fetch_zval_from_symbol_table(st, keyword, keyword_end - keyword, type, current_classname, cc_length TSRMLS_CC);
+								if (current_classname) {
+									efree(current_classname);
+								}
+								current_classname = NULL;
 								if (retval) {
-									if (current_classname) {
-										efree(current_classname);
-									}
 									current_classname = fetch_classname_from_zval(retval, &cc_length TSRMLS_CC);
 									st = fetch_ht_from_zval(retval TSRMLS_CC);
 								}
@@ -503,6 +504,7 @@ static zval* get_symbol_contents_zval(char* name, int name_length TSRMLS_DC)
 							}
 							current_classname = NULL;
 							if (retval) {
+								current_classname = fetch_classname_from_zval(retval, &cc_length TSRMLS_CC);
 								st = fetch_ht_from_zval(retval TSRMLS_CC);
 							}
 							keyword = NULL;
@@ -521,8 +523,9 @@ static zval* get_symbol_contents_zval(char* name, int name_length TSRMLS_DC)
 							if (current_classname) {
 								efree(current_classname);
 							}
-							current_classname = fetch_classname_from_zval(retval, &cc_length TSRMLS_CC);
+							current_classname = NULL;
 							if (retval) {
+								current_classname = fetch_classname_from_zval(retval, &cc_length TSRMLS_CC);
 								st = fetch_ht_from_zval(retval TSRMLS_CC);
 							}
 							keyword = NULL;
@@ -2118,7 +2121,7 @@ int xdebug_dbgp_parse_option(xdebug_con *context, char* line, int flags, xdebug_
 
 char *xdebug_dbgp_get_revision(void)
 {
-	return "$Revision: 1.107 $";
+	return "$Revision: 1.108 $";
 }
 
 int xdebug_dbgp_cmdloop(xdebug_con *context TSRMLS_DC)
