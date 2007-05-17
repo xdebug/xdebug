322654dce04f4b7c655c243fdff37a33a528989a
Derick Rethans <derick@derickrethans.nl>
2007-05-17 15:26:18 +0000

    - GO with RC4.
    SVN Rev: 2342

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,7 +20,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "2.0.0RC4-dev"
+#define XDEBUG_VERSION    "2.0.0RC4"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2007 by Derick Rethans"
 #define XDEBUG_URL        "http://xdebug.org"
