842365cd3278cca14a759bb22daeac327309644a
Derick Rethans <derick@derickrethans.nl>
2007-07-08 18:33:45 +0000

    - Fixed bug #289: Xdebug terminates connection when eval() is run in the init   stage.
    SVN Rev: 2432

--- xdebug.c
+++ xdebug.c
@@ -1403,18 +1403,19 @@ void xdebug_execute(zend_op_array *op_array TSRMLS_DC)
 			/* Initialize debugging session */
 			XG(context).socket = xdebug_create_socket(XG(remote_host), XG(remote_port));
 			if (XG(context).socket >= 0) {
-				XG(remote_enabled) = 0;
+				XG(remote_enabled) = 1;
 
 				/* Get handler from mode */
 				XG(context).handler = xdebug_handler_get(XG(remote_handler));
 				if (!XG(context).handler) {
 					zend_error(E_WARNING, "The remote debug handler '%s' is not supported.", XG(remote_handler));
+					XG(remote_enabled) = 0;
 				} else if (!XG(context).handler->remote_init(&(XG(context)), XDEBUG_REQ)) {
 					/* The request could not be started, ignore it then */
+					XG(remote_enabled) = 0;
 				} else {
 					/* All is well, turn off script time outs */
 					zend_alter_ini_entry("max_execution_time", sizeof("max_execution_time"), "0", strlen("0"), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
-					XG(remote_enabled) = 1;
 				}
 			}
 		}
