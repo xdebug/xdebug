66474b4d1b84ee7c40d8c7760306e3e02e2e00fd
Derick Rethans <derick@derickrethans.nl>
2007-07-18 17:25:32 +0000

    - Fixed test script.
    SVN Rev: 2450

--- test-4.3.sh
+++ test-4.3.sh
@@ -1,1 +1,1 @@
-TEST_PHP_EXECUTABLE=`which php-4.4dev` php-5.2dev /home/derick/dev/php/php-5.2dev/run-tests.php tests/*.phpt
+TEST_PHP_EXECUTABLE=`which php-4.3dev` php-5.2dev /home/derick/dev/php/php-5.2dev/run-tests.php tests/*.phpt
