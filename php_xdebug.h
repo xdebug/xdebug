1645418b823093907f875e4126e39477704c6ee4
Derick Rethans <derick@derickrethans.nl>
2008-12-30 15:50:39 +0000

    - Back to -dev.
    SVN Rev: 2886

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,7 +20,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "2.0.4"
+#define XDEBUG_VERSION    "2.0.5-dev"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2008 by Derick Rethans"
 #define XDEBUG_URL        "http://xdebug.org"
