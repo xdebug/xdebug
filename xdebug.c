ad284abb69db47aa81e84247e6a7d89d5690d075
Derick Rethans <derick@derickrethans.nl>
2009-07-09 16:52:36 +0000

    - Update year here as well.
    SVN Rev: 3033

--- xdebug.c
+++ xdebug.c
@@ -3170,7 +3170,7 @@ ZEND_DLEXPORT zend_extension zend_extension_entry = {
 	XDEBUG_VERSION,
 	XDEBUG_AUTHOR,
 	XDEBUG_URL,
-	"Copyright (c) 2002-2008",
+	"Copyright (c) 2002-2009",
 	xdebug_zend_startup,
 	xdebug_zend_shutdown,
 	NULL,           /* activate_func_t */
