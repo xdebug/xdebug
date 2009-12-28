906122cdb629b39db72ba7907c34d3cde2c253b1
Derick Rethans <derick@derickrethans.nl>
2009-12-28 17:31:25 +0000

    - Fixed testing scripts.
    SVN Rev: 3166

--- test-5.1.sh
+++ test-5.1.sh
@@ -1,1 +1,1 @@
-TEST_PHP_EXECUTABLE=`which php-5.1dev` php-5.1dev -dxdebug.auto_trace=0 /home/derick/dev/php/php-src/branches/PHP_5_1/run-tests.php tests/*.phpt
+TEST_PHP_EXECUTABLE=`which php` php -dxdebug.auto_trace=0 /home/derick/dev/php/php-src/branches/PHP_5_1/run-tests.php tests/*.phpt
--- test-5.2.sh
+++ test-5.2.sh
@@ -1,1 +1,1 @@
-TEST_PHP_EXECUTABLE=`which php-5.2dev` php-5.2dev -dxdebug.auto_trace=0 /home/derick/dev/php/php-src/branches/PHP_5_2/run-tests.php tests/*.phpt
+TEST_PHP_EXECUTABLE=`which php` php -dxdebug.auto_trace=0 /home/derick/dev/php/php-src/branches/PHP_5_2/run-tests.php tests/*.phpt
