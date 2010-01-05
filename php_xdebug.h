3d379847494643c907cbd8e3b0ec0494196ae8e4
Derick Rethans <derick@derickrethans.nl>
2010-01-05 14:03:37 +0000

    - Back to -dev
    SVN Rev: 3203

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,7 +20,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "2.1.0beta1"
+#define XDEBUG_VERSION    "2.1.0beta2-dev"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2010 by Derick Rethans"
 #define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2010"
