f11fe7ae9571c314d6e3e496e7eae6d7e5bf9f30
Derick Rethans <derick@derickrethans.nl>
2011-03-27 11:56:01 +0000

    - Go with 2.1.1
    SVN Rev: 3427

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,7 +20,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "2.1.1rc2-dev"
+#define XDEBUG_VERSION    "2.1.1"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2011 by Derick Rethans"
 #define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2011"
