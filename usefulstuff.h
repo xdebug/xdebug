a44d888c0571b7fc6e7e8600a1d0c429a7b07a2c
Patrick Allaert <pa@ez.no>
2012-03-09 14:58:51 +0100

    Fixed: Implicit declarations of some functions.
    Avoid warnings like: "implicit declaration of function '...'"

--- usefulstuff.h
+++ usefulstuff.h
@@ -61,6 +61,8 @@ char *xdebug_path_to_url(const char *fileurl TSRMLS_DC);
 char *xdebug_path_from_url(const char *fileurl TSRMLS_DC);
 FILE *xdebug_fopen(char *fname, char *mode, char *extension, char **new_fname);
 int xdebug_format_output_filename(char **filename, char *format, char *script_name);
+void xdebug_open_log(TSRMLS_D);
+void xdebug_close_log(TSRMLS_D);
 
 #define XDEBUG_CRC32(crc, ch)	 (crc = (crc >> 8) ^ xdebug_crc32tab[(crc ^ (ch)) & 0xff])
 
