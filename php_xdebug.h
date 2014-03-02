c3af0f9e80367114f5bb67f4bcff76d2ce7eaf68
Derick Rethans <github@derickrethans.nl>
2014-03-02 14:36:38 -0500

    Go with 2.2.4.

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,10 +20,10 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "2.2.4dev"
+#define XDEBUG_VERSION    "2.2.4"
 #define XDEBUG_AUTHOR     "Derick Rethans"
-#define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2013 by Derick Rethans"
-#define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2013"
+#define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2014 by Derick Rethans"
+#define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2014"
 #define XDEBUG_URL        "http://xdebug.org"
 #define XDEBUG_URL_FAQ    "http://xdebug.org/docs/faq#api"
 
