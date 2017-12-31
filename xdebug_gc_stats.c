e7b81ae201c579ef0535c2fa416d67a6e75db9ab
Derick Rethans <github@derickrethans.nl>
2017-12-31 14:52:27 +0000

    Remove unused defines

--- xdebug_gc_stats.c
+++ xdebug_gc_stats.c
@@ -16,11 +16,6 @@
    +----------------------------------------------------------------------+
  */
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
-
-#include "php.h"
 #include "php_xdebug.h"
 #include "xdebug_gc_stats.h"
 #include "xdebug_stack.h"
