7885bfa2d51ea4da3cee4fb626808b18603b536e
Derick Rethans <github@derickrethans.nl>
2018-01-07 20:11:45 +0000

    Fixed issue #1511: Detect and use compiler flags through new configure option

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-export CFLAGS="-Wall -Werror -Wextra -Wmaybe-uninitialized -Wdeclaration-after-statement -Wmissing-field-initializers -Wshadow -Wno-unused-parameter -ggdb3"
-phpize && ./configure && make clean && make -j 5 all && make install
+phpize && ./configure --enable-xdebug-dev && make clean && make -j 5 all && make install
