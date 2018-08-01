62f07406c53dc5a806f8dd73af54d11b50d3d38b
Derick Rethans <github@derickrethans.nl>
2018-08-01 23:44:43 +0100

    Back to -dev

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,7 +20,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "2.6.1"
+#define XDEBUG_VERSION    "2.6.2-dev"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2018 by Derick Rethans"
 #define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2018"
