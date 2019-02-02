777fbb7711569c57320e700ca842f312a88e0d86
Derick Rethans <github@derickrethans.nl>
2019-02-02 17:33:03 +0000

    Fixed whitespace

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -4,7 +4,7 @@ for i in /usr/local/php/*; do
 	version=`echo $i | sed 's@.*/@@'`
 	echo -n "Rebuilding for PHP ${version}: "
 
-    export PATH=/usr/local/php/${version}/bin:/usr/lib/ccache:/usr/local/bin:/usr/bin:/bin
+	export PATH=/usr/local/php/${version}/bin:/usr/lib/ccache:/usr/local/bin:/usr/bin:/bin
 
 	if [[ "${version}" =~ "32bit" ]]; then
 		echo -n "(in 32-bit mode) "
