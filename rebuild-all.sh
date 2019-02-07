28660ac3ad1abf893c09c363af1f991047c3e67f
Derick Rethans <github@derickrethans.nl>
2019-02-07 14:36:41 +0000

    Improve rebuild-all.sh script a little, by allowing for only new versions to be compiled

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+FORCE=${1:-"YES"}
+
+if [[ ${FORCE} == "YES" ]]; then
+	echo "Forcing builds for all versions"
+fi
+
 for i in /usr/local/php/*; do
 	version=`echo $i | sed 's@.*/@@'`
 	echo -n "Rebuilding for PHP ${version}: "
 
-	export PATH=/usr/local/php/${version}/bin:/usr/lib/ccache:/usr/local/bin:/usr/bin:/bin
+	FILE=/usr/local/php/${version}/lib/php/extensions/*/xdebug.so
 
-	./rebuild.sh >/tmp/xdebug-for-${version}.log 2>&1
+	if [ ! -e $FILE ] || [[ ${FORCE} == "YES" ]]; then
+		export PATH=/usr/local/php/${version}/bin:/usr/lib/ccache:/usr/local/bin:/usr/bin:/bin
 
-	if [[ $? == 0 ]]; then
-		echo "DONE"
+		LOG="/tmp/xdebug-for-${version}.log"
+		echo -n "Building: "
+		./rebuild.sh >$LOG 2>&1
+
+		if [[ $? == 0 ]]; then
+			echo "DONE"
+		else
+			echo "FAIL, log is at $LOG"
+		fi
 	else
-		echo "FAIL"
+		echo "SKIP"
 	fi
 done
