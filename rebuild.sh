86fda95b108f531024fb30efd6ebd4d4083c1535
Derick Rethans <github@derickrethans.nl>
2019-05-19 18:03:43 +0100

    Remove configure.{in,ac} files before phpize

--- rebuild.sh
+++ rebuild.sh
@@ -5,4 +5,6 @@ bit64=`php -n -r 'echo PHP_INT_SIZE == 8 ? "1" : "0";'`
 if [[ ${bit64} != "1" ]]; then
 	export CFLAGS="-m32"
 fi
+rm configure.in
+rm configure.ac
 phpize && ./configure --enable-xdebug-dev && make clean && make all && make install
