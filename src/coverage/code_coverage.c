57b58c62d454f8af430f05b72162bf587db70109
Derick Rethans <github@derickrethans.nl>
2020-03-10 11:08:32 +0000

    PHP 8 Compat: zend_hash_sort compare function signature change

--- src/coverage/code_coverage.c
+++ src/coverage/code_coverage.c
@@ -637,12 +637,15 @@ PHP_FUNCTION(xdebug_stop_code_coverage)
 	RETURN_FALSE;
 }
 
-
+#if PHP_VERSION_ID >= 80000
+static int xdebug_lineno_cmp(Bucket *f, Bucket *s)
+{
+#else
 static int xdebug_lineno_cmp(const void *a, const void *b)
 {
 	Bucket *f = (Bucket *) a;
 	Bucket *s = (Bucket *) b;
-
+#endif
 	if (f->h < s->h) {
 		return -1;
 	} else if (f->h > s->h) {
