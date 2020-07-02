296f31d52e71ab516864715ef86d537af9446ab5
Derick Rethans <github@derickrethans.nl>
2020-07-02 12:07:24 +0100

    Colourize run-xdebug-tests.php output

--- run-xdebug-tests.php
+++ run-xdebug-tests.php
@@ -2722,7 +2722,18 @@ function show_result($result, $tested, $tested_file, $extra = '', $temp_filename
 	global $html_output, $html_file, $temp_target, $temp_urlbase, $line_length, $SHOW_ONLY_GROUPS;
 
 	if (!$SHOW_ONLY_GROUPS || in_array($result, $SHOW_ONLY_GROUPS)) {
-		echo "$result $tested [$tested_file] $extra\n";
+		switch ( $result ) {
+			case 'PASS':
+				$colour = "\e[1;32m{$result}\e[0m"; break;
+			case 'SKIP':
+				$colour = "\e[0;37m{$result}"; break;
+			case 'FAIL':
+				$colour = "\e[1;31m{$result}\e[0m"; break;
+			default:
+				$colour = "\e[1;33m{$result}\e[0m"; break;
+		}
+
+		echo "$colour $tested [$tested_file] $extra\e[0m\n";
 	} else if (!$SHOW_ONLY_GROUPS) {
 		// Write over the last line to avoid random trailing chars on next echo
 		echo str_repeat(" ", $line_length), "\r";
