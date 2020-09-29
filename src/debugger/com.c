86b855f7ab8d0230ae1dbee96b587176314245a1
Calvin Buckley <calvin@cmpct.info>
2020-09-29 13:15:40 -0400

    AIX needs this alias too
    It might be prudent to just use this definition unconditionally.

--- src/debugger/com.c
+++ src/debugger/com.c
@@ -87,7 +87,7 @@ static int xdebug_create_socket_unix(const char *path)
 #if !WIN32 && !WINNT
 
 /* For OSX and FreeBSD */
-#if !defined(SOL_TCP) && defined(IPPROTO_TCP) && (defined(__APPLE__) || defined(__FreeBSD__))
+#if !defined(SOL_TCP) && defined(IPPROTO_TCP) && (defined(__APPLE__) || defined(__FreeBSD__) || defined(_AIX))
 # define SOL_TCP IPPROTO_TCP
 #endif
 #if !defined(TCP_KEEPIDLE) && defined(TCP_KEEPALIVE) && defined(__APPLE__)
