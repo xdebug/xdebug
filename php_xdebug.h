10dd57bd89984d46fa7b97568dbb0987d3f2dbf7
Derick Rethans <github@derickrethans.nl>
2020-11-26 17:01:14 +0000

    Back to -dev in new master (3.1) branch

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,7 +20,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "3.0.0"
+#define XDEBUG_VERSION    "3.1.0-dev"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2020 by Derick Rethans"
 #define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2020"
