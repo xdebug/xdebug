3d41ef03a6061cb1f432ce4b707ce837ca9a2913
Calvin Buckley <calvin@cmpct.info>
2020-11-27 10:43:40 -0500

    Fixed issue #1902: Compillation failure on AIX
    Otherwise, GCC emits the following error:
    
    ```
    /home/calvin/rpmbuild/BUILD/xdebug-3.0.0/src/lib/timing.h:34:29: error: missing binary operator before token "10"
     if PHP_WIN32 | __APPLE__ | CLOCK_MONOTONIC
                                 ^
    ```
    
    (# omitted because git)

--- src/lib/timing.h
+++ src/lib/timing.h
@@ -31,7 +31,7 @@ typedef void (WINAPI *WIN_PRECISE_TIME_FUNC)(LPFILETIME);
 typedef struct _xdebug_nanotime_context {
 	uint64_t start_abs;
 	uint64_t last_abs;
-#if PHP_WIN32 | __APPLE__ | CLOCK_MONOTONIC
+#if PHP_WIN32 | __APPLE__ | defined(CLOCK_MONOTONIC)
 	uint64_t start_rel;
 	uint64_t last_rel;
 	int      use_rel_time;
