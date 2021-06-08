9d9e9b3c3db353adb296915d4398fb64f13e5ab2
Derick Rethans <github@derickrethans.nl>
2021-06-08 10:35:54 +0100

    Fixed issue #1674: Inconsistent Path & Branch Coverage Reported
    (cherry picked from commit c3dafbb349e22f8421f28d69ee4ffe262d9f19be)

--- src/coverage/branch_info.h
+++ src/coverage/branch_info.h
@@ -2,7 +2,7 @@
    +----------------------------------------------------------------------+
    | Xdebug                                                               |
    +----------------------------------------------------------------------+
-   | Copyright (c) 2002-2020 Derick Rethans                               |
+   | Copyright (c) 2002-2025 Derick Rethans                               |
    +----------------------------------------------------------------------+
    | This source file is subject to version 1.01 of the Xdebug license,   |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -32,23 +32,21 @@
 #define XDEBUG_JMP_NOT_SET (INT_MAX-1)
 #define XDEBUG_JMP_EXIT    (INT_MAX-2)
 
-#define XDEBUG_BRANCH_MAX_OUTS 64
+#define XDEBUG_MAX_PATHS       4096
+#define XDEBUG_BRANCH_MAX_OUTS   64
 
 typedef struct _xdebug_branch {
 	unsigned int  start_lineno;
 	unsigned int  end_lineno;
 	unsigned int  end_op;
-	unsigned char hit;
 	unsigned int  outs_count;
 	int           outs[XDEBUG_BRANCH_MAX_OUTS];
-	unsigned char outs_hit[XDEBUG_BRANCH_MAX_OUTS];
 } xdebug_branch;
 
 typedef struct _xdebug_path {
 	unsigned int elements_count;
 	unsigned int elements_size;
 	unsigned int *elements;
-	unsigned char hit;
 } xdebug_path;
 
 /* Contains information for paths that belong to a set of branches (as stored in xdebug_branch_info) */
@@ -66,31 +64,8 @@ typedef struct _xdebug_branch_info {
 	xdebug_set      *starts;   /* A set of opcodes nrs where each branch starts */
 	xdebug_set      *ends;     /* A set of opcodes nrs where each ends starts */
 	xdebug_branch   *branches; /* Information about each branch */
+	size_t           highest_out;
 
 	xdebug_path_info path_info; /* The paths that can be created out of these branches */
 } xdebug_branch_info;
-
-xdebug_branch_info *xdebug_branch_info_create(unsigned int size);
-
-void xdebug_branch_info_update(xdebug_branch_info *branch_info, unsigned int pos, unsigned int lineno, unsigned int outidx, unsigned int jump_pos);
-void xdebug_branch_post_process(zend_op_array *opa, xdebug_branch_info *branch_info);
-void xdebug_branch_find_paths(xdebug_branch_info *branch_info);
-
-void xdebug_branch_info_dump(zend_op_array *opa, xdebug_branch_info *branch_info);
-void xdebug_branch_info_add_branches_and_paths(zend_string *filename, char *function_name, xdebug_branch_info *branch_info);
-void xdebug_branch_info_free(xdebug_branch_info *branch_info);
-
-xdebug_path *xdebug_path_new(xdebug_path *old_path);
-void xdebug_path_free(xdebug_path *path);
-
-xdebug_path_info *xdebug_path_info_ctor(void);
-void xdebug_path_info_dtor(xdebug_path_info *path_info);
-
-void xdebug_path_info_add_path_for_level(xdebug_path_info *path_info, xdebug_path *path, unsigned int level);
-xdebug_path *xdebug_path_info_get_path_for_level(xdebug_path_info *path_info, unsigned int level);
-
-void xdebug_create_key_for_path(xdebug_path *path, xdebug_str *str);
-
-void xdebug_branch_info_mark_reached(zend_string *filename, char *function_name, zend_op_array *op_array, long opcode_nr);
-void xdebug_branch_info_mark_end_of_function_reached(zend_string *filename, char *function_name, char *key, int key_len);
 #endif
