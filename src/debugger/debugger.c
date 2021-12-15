432af929f364facabb7747afbd2a128205468d78
Derick Rethans <github@derickrethans.nl>
2021-12-15 11:37:17 +0000

    Mark 'int res' as volatile, as that seems to be needed on PHP 7.4

--- src/debugger/debugger.c
+++ src/debugger/debugger.c
@@ -166,7 +166,7 @@ int xdebug_do_eval(char *eval_string, zval *ret_zval)
 #if PHP_VERSION_ID < 80000
 	int                old_track_errors;
 #endif
-	int                res = 1;
+	volatile int       res = 1;
 	zend_execute_data *original_execute_data = EG(current_execute_data);
 	int                original_no_extensions = EG(no_extensions);
 	zend_object       *original_exception = EG(exception);
