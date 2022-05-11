2b0e2a6473f0655bf09c5b45f14976792f1de306
Ray Perea <ray@consta.link>
2022-05-11 00:50:20 -0500

    Fixed bug #2085: Crash when used with source guardian encoded files

--- src/base/filter.c
+++ src/base/filter.c
@@ -60,7 +60,7 @@ void xdebug_filter_register_constants(INIT_FUNC_ARGS)
 
 static int xdebug_filter_match_path_include(function_stack_entry *fse, unsigned char *filtered_flag, char *filter)
 {
-	if (strncasecmp(filter, ZSTR_VAL(fse->filename), strlen(filter)) == 0) {
+	if (fse->filename && strncasecmp(filter, ZSTR_VAL(fse->filename), strlen(filter)) == 0) {
 		*filtered_flag = 0;
 		return 1;
 	}
@@ -69,7 +69,7 @@ static int xdebug_filter_match_path_include(function_stack_entry *fse, unsigned 
 
 static int xdebug_filter_match_path_exclude(function_stack_entry *fse, unsigned char *filtered_flag, char *filter)
 {
-	if (strncasecmp(filter, ZSTR_VAL(fse->filename), strlen(filter)) == 0) {
+	if (fse->filename && strncasecmp(filter, ZSTR_VAL(fse->filename), strlen(filter)) == 0) {
 		*filtered_flag = 1;
 		return 1;
 	}
