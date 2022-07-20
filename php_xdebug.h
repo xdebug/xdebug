c7603a311eda595dcbd82e755cf624788f9c437e
Derick Rethans <github@derickrethans.nl>
2022-07-20 16:55:08 +0100

    Create xdebug_3_2 branch

--- php_xdebug.h
+++ php_xdebug.h
@@ -20,8 +20,8 @@
 #define XDEBUG_NAME       "Xdebug"
 #define XDEBUG_VERSION    "3.2.0-dev"
 #define XDEBUG_AUTHOR     "Derick Rethans"
-#define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2021 by Derick Rethans"
-#define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2021"
+#define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2022 by Derick Rethans"
+#define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2022"
 #define XDEBUG_URL        "https://xdebug.org"
 #define XDEBUG_URL_FAQ    "https://xdebug.org/docs/faq#api"
 
