7e929a23b959402ad2bd847ccf2d3306dbd16210
Derick Rethans <github@derickrethans.nl>
2023-12-06 16:45:20 +0000

    Master branch is now for 3.4

--- php_xdebug.h
+++ php_xdebug.h
@@ -18,7 +18,7 @@
 #define PHP_XDEBUG_H
 
 #define XDEBUG_NAME       "Xdebug"
-#define XDEBUG_VERSION    "3.3.0"
+#define XDEBUG_VERSION    "3.4.0-dev"
 #define XDEBUG_AUTHOR     "Derick Rethans"
 #define XDEBUG_COPYRIGHT  "Copyright (c) 2002-2023 by Derick Rethans"
 #define XDEBUG_COPYRIGHT_SHORT "Copyright (c) 2002-2023"
