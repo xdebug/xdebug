4bf0c14f2926524d4722f8c87a8cfc26ae513bb6
Derick Rethans <github@derickrethans.nl>
2024-01-23 17:52:08 +0000

    No longer test for PHP 7 in Xdebug CI

--- .xdebugci/build-nightly-php.sh
+++ .xdebugci/build-nightly-php.sh
@@ -13,4 +13,4 @@ cd ${DIR}/..
 
 PATH=/usr/local/php/7.3.6/bin:$PATH
 
-PHP_DIR=/tmp/php-build/workspace/install .xdebugci/test-all.sh '^master|^7\.[^01].*dev|^8.*dev'
+PHP_DIR=/tmp/php-build/workspace/install .xdebugci/test-all.sh '^master|^8.*dev'
