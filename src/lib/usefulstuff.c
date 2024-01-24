7cb3c044f3b74bb8f46b1d06a6ab76ec3d48c3ff
Derick Rethans <github@derickrethans.nl>
2024-01-24 13:08:09 +0000

    PHP 8.4: Include new(er) header for random, as the old has been removed

--- src/lib/usefulstuff.c
+++ src/lib/usefulstuff.c
@@ -41,7 +41,11 @@
 #include "usefulstuff.h"
 #include "log.h"
 
-#include "ext/standard/php_lcg.h"
+#if PHP_VERSION_ID >= 80200
+# include "ext/random/php_random.h"
+#else
+# include "ext/standard/php_lcg.h"
+#endif
 #include "ext/standard/flock_compat.h"
 #include "main/php_ini.h"
 
