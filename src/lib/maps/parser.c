d9d25280389fc9570cb4833d6f9aaf6b4c967bab
Derick Rethans <github@derickrethans.nl>
2024-07-26 17:54:47 +0100

    Implement very naive parsing of files
    - It parses the remote/local prefixes, but ignores them
    - It adds rules to a hash, but not properly trimmed or tested
    - No API (nor test) to check whether rules work
    - No tests or checks for different types of rules (directory, file, file+range)

--- src/lib/maps/parser.c
+++ src/lib/maps/parser.c
@@ -14,59 +14,184 @@
    +----------------------------------------------------------------------+
  */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "maps_private.h"
+#include "parser.h"
+
+#include "../hash.h"
+#include "../mm.h"
+#include "../trim.h"
+#include "../xdebug_strndup.h"
 
 typedef struct path_maps_parser_state {
+	int   error_code;
+	char *error_message;
+
 	char *current_local_prefix;
 	char *current_remote_prefix;
 
 	FILE *current_file;
-} path_maps_parser_state;
 
+	xdebug_hash *file_rules;
+} path_maps_parser_state;
 
 static void path_maps_parser_state_ctor(struct path_maps_parser_state *state)
 {
+	state->error_code = PATH_MAPS_OK;
+	state->error_message = NULL;
+
 	state->current_file = NULL;
 	state->current_local_prefix = NULL;
 	state->current_remote_prefix = NULL;
+
+	state->file_rules = xdebug_hash_alloc(256, xdebug_path_mapping_free);
 }
 
 static void path_maps_parser_state_dtor(struct path_maps_parser_state *state)
 {
+	state->error_code = PATH_MAPS_OK;
+	if (state->error_message) {
+		xdfree(state->error_message);
+	}
+	if (state->current_local_prefix) {
+		xdfree(state->current_local_prefix);
+		state->current_local_prefix = NULL;
+	}
+	if (state->current_remote_prefix) {
+		xdfree(state->current_remote_prefix);
+		state->current_remote_prefix = NULL;
+	}
 	if (state->current_file) {
 		fclose(state->current_file);
 		state->current_file = NULL;
 	}
-	if (state->current_local_prefix) {
-		free(state->current_local_prefix);
-		state->current_local_prefix = NULL;
-	}
-	if (state->current_remote_prefix) {
-		free(state->current_remote_prefix);
-		state->current_remote_prefix = NULL;
+	if (state->file_rules) {
+		xdebug_hash_destroy(state->file_rules);
 	}
 }
 
-static FILE *path_maps_parser_open_file(const char *filename, int *error_code, char **error_message)
+static void state_set_error(path_maps_parser_state *state, int error_code, const char *error_message)
 {
-	FILE *tmp;
+	state->error_code = error_code;
+	if (state->error_message) {
+		xdfree(state->error_message);
+	}
+	state->error_message = xdstrdup(error_message);
+}
 
-	tmp = fopen(filename, "r");
+static int state_get_error_code(path_maps_parser_state *state)
+{
+	return state->error_code;
+}
+
+static char *state_get_error_message(path_maps_parser_state *state)
+{
+	return xdstrdup(state->error_message);
+}
+
+static bool state_open_file(path_maps_parser_state *state, const char *filename)
+{
+	FILE *tmp = fopen(filename, "r");
 
 	if (!tmp) {
-		*error_code = PATH_MAPS_CANT_OPEN_FILE;
-		*error_message = strdup("Can't open file");
-		return NULL;
+		state_set_error(state, PATH_MAPS_CANT_OPEN_FILE, "Can't open file");
+		return false;
 	}
 
-	return tmp;
+	state->current_file = tmp;
+	return true;
 }
 
+static void state_set_remote_prefix(path_maps_parser_state *state, const char *prefix)
+{
+	state->current_remote_prefix = xdebug_trim(prefix);
+}
+
+static void state_set_local_prefix(path_maps_parser_state *state, const char *prefix)
+{
+	state->current_local_prefix = xdebug_trim(prefix);
+}
+
+static bool state_add_rule(path_maps_parser_state *state, const char *buffer, const char *equals)
+{
+	xdebug_path_mapping* tmp = (xdebug_path_mapping*) xdcalloc(1, sizeof(xdebug_path_mapping));
+
+	tmp->remote_path = xdebug_strndup(buffer, equals - buffer - 1);
+	tmp->local_path = xdebug_trim(equals + 1);
+	tmp->type = XDEBUG_PATH_MAP_TYPE_DIRECTORY;
+
+	xdebug_hash_add(state->file_rules, buffer, equals - buffer, tmp);
+	return true;
+}
+
+static bool state_file_read_lines(path_maps_parser_state *state)
+{
+	char   buffer[2048];
+	size_t current_line_no = 0;
+
+	while (fgets(buffer, sizeof(buffer), state->current_file) != NULL) {
+		size_t  buffer_len;
+		char   *equals;
+
+		++current_line_no;
+
+		/* if last char is no \n, abort */
+		buffer_len = strlen(buffer);
+
+		/* empty line */
+		if (buffer_len == 0) {
+			state_set_error(state, PATH_MAPS_EMPTY_LINE, "Line XXX is empty, that shouldn't be possible");
+			return false;
+		}
+		if (buffer[buffer_len - 1] != '\n') {
+			state_set_error(state, PATH_MAPS_NO_NEWLINE, "Line XXX does not end in a new line");
+			return false;
+		}
+
+		/* stanzas */
+		if (strncmp(buffer, "remote_prefix:", strlen("remote_prefix:")) == 0) {
+			state_set_remote_prefix(state, buffer + strlen("remote_prefix:"));
+			continue;
+		}
+		if (strncmp(buffer, "local_prefix:", strlen("local_prefix:")) == 0) {
+			state_set_local_prefix(state, buffer + strlen("local_prefix:"));
+			continue;
+		}
+
+		/* comments */
+		if (buffer[0] == '#') {
+			continue;
+		}
+
+		/* assignments */
+		equals = strchr(buffer, '=');
+		if (!equals) {
+			state_set_error(state, PATH_MAPS_GARBAGE, "Line XXX is not empty and has no stanza, assignment, or starts with '#'");
+			return false;
+		}
+
+		state_add_rule(state, buffer, equals);
+	}
+
+	return true;
+}
+
+static void copy_rule(void *ret, xdebug_hash_element *e)
+{
+	xdebug_path_mapping *new_rule = (xdebug_path_mapping*) e->ptr;
+	xdebug_path_mapping *tmp = (xdebug_path_mapping*) xdmalloc(sizeof(xdebug_path_mapping));
+
+	tmp->local_path = xdstrdup(new_rule->local_path);
+	tmp->remote_path = xdstrdup(new_rule->remote_path);
+	tmp->type = new_rule->type;
+
+	xdebug_hash_add((xdebug_hash*) ret, e->key.value.str.val, e->key.value.str.len - 1, tmp);
+}
 
 /* Parses a path mapping file.
  *
@@ -79,15 +204,28 @@ bool xdebug_path_maps_parse_file(xdebug_path_maps *maps, const char *filename, i
 
 	path_maps_parser_state_ctor(&state);
 
-	state.current_file = path_maps_parser_open_file(filename, error_code, error_message);
-	if (!state.current_file) {
+	if (!state_open_file(&state, filename)) {
 		goto error;
 	}
+
+	if (!state_file_read_lines(&state)) {
+		goto error;
+	}
+
+	if (state.file_rules->size == 0) {
+		state_set_error(&state, PATH_MAPS_NO_RULES, "The map file did not provide any mappings");
+		goto error;
+	}
+
+	/* No errors, so copy all rules to actual map */
+	xdebug_hash_apply(state.file_rules, (void*) maps->remote_to_local_map, copy_rule);
 
 	path_maps_parser_state_dtor(&state);
 	return true;
 
 error:
+	*error_code = state_get_error_code(&state);
+	*error_message = state_get_error_message(&state);
 	path_maps_parser_state_dtor(&state);
 	return false;
 }
