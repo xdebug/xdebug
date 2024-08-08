c7f8d5edfda571e89e9e53fa6b65f88b2eb02674
Derick Rethans <github@derickrethans.nl>
2024-08-08 16:29:20 +0100

    Move from file-based testing, to embedded strings

--- tests/ctest/path_map_test.cpp
+++ tests/ctest/path_map_test.cpp
@@ -1,5 +1,7 @@
 #include "CppUTest/TestHarness.h"
 
+#include <unistd.h>
+
 #include "maps_private.h"
 #include "maps.h"
 #include "parser.h"
@@ -11,6 +13,9 @@ TEST_GROUP(path_maps_file)
 	int   error_code;
 	char *error_message;
 	size_t mapping_count;
+	FILE *filep;
+	xdebug_path_mapping *mapping;
+	char *filename;
 
 	TEST_SETUP()
 	{
@@ -18,12 +23,55 @@ TEST_GROUP(path_maps_file)
 		result = false;
 		error_code = PATH_MAPS_OK;
 		error_message = NULL;
+		filename = NULL;
+		filep = NULL;
+		mapping = NULL;
+	}
+
+	bool test_map_from_file(const char *data_string)
+	{
+		char templ[] = "/tmp/xdct.XXXXXX";
+
+		int fp = mkstemp(templ);
+
+		filep = fdopen(fp, "w");
+		fputs(data_string, filep);
+		fclose(filep);
+		filep = NULL;
+
+		filename = strdup(templ);
+
+		return xdebug_path_maps_parse_file(test_map, filename, &error_code, &error_message);
+	}
+
+	void check_result(size_t expected_error_code, size_t result_count)
+	{
+		LONGS_EQUAL(expected_error_code == PATH_MAPS_OK ? true : false, result);
+		LONGS_EQUAL(expected_error_code, error_code);
+		LONGS_EQUAL(result_count, xdebug_path_maps_get_rule_count(test_map));
+	}
+
+	void check_map(size_t type, const char *remote_path, const char *local_path)
+	{
+		CHECK(mapping);
+		LONGS_EQUAL(type, mapping->type);
+		STRCMP_EQUAL(remote_path, mapping->remote_path);
+		STRCMP_EQUAL(local_path, mapping->local_path);
 	}
 
 	TEST_TEARDOWN()
 	{
 		if (error_message) {
 			free(error_message);
+		}
+
+		if (filename) {
+			unlink(filename);
+			free(filename);
+			filename = NULL;
+		}
+		if (filep) {
+			fclose(filep);
 		}
 
 		xdebug_path_maps_dtor(test_map);
@@ -39,68 +87,63 @@ TEST(path_maps_file, fopen_non_existing)
 	STRCMP_EQUAL("Can't open file", error_message);
 };
 
-TEST(path_maps_file, fopen_existing)
-{
-	result = xdebug_path_maps_parse_file(test_map, "files/simple.map", &error_code, &error_message);
-
-	LONGS_EQUAL(true, result);
-	LONGS_EQUAL(PATH_MAPS_OK, error_code);
-};
-
 TEST(path_maps_file, empty)
 {
-	result = xdebug_path_maps_parse_file(test_map, "files/empty.map", &error_code, &error_message);
+	const char *map = R""""()"""";
 
-	LONGS_EQUAL(false, result);
-	LONGS_EQUAL(PATH_MAPS_NO_RULES, error_code);
-	STRCMP_EQUAL("The map file did not provide any mappings", error_message);
+	result = test_map_from_file(map);
+	check_result(PATH_MAPS_NO_RULES, 0);
 };
 
 TEST(path_maps_file, no_rules)
 {
-	result = xdebug_path_maps_parse_file(test_map, "files/no-rules.map", &error_code, &error_message);
+	const char *map = R""""(
+remote_prefix: /var/www/
+local_prefix: /home/derick/projects/example.com/
+)"""";
 
+	result = test_map_from_file(map);
+	check_result(PATH_MAPS_NO_RULES, 0);
 	LONGS_EQUAL(false, result);
-	LONGS_EQUAL(PATH_MAPS_NO_RULES, error_code);
 	STRCMP_EQUAL("The map file did not provide any mappings", error_message);
 };
 
 TEST(path_maps_file, full_path_map)
 {
-	result = xdebug_path_maps_parse_file(test_map, "files/simple.map", &error_code, &error_message);
+	const char *map = R""""(
+/var/www/ = /home/derick/projects/example.com/
+)"""";
 
-	LONGS_EQUAL(true, result);
-	LONGS_EQUAL(PATH_MAPS_OK, error_code);
-	LONGS_EQUAL(1, xdebug_path_maps_get_rule_count(test_map));
+	result = test_map_from_file(map);
+	check_result(PATH_MAPS_OK, 1);
 };
 
 TEST(path_maps_file, check_rules)
 {
-	xdebug_path_mapping *mapping = NULL;
-	result = xdebug_path_maps_parse_file(test_map, "files/simple.map", &error_code, &error_message);
+	const char *map = R""""(
+/var/www/ = /home/derick/projects/example.com/
+)"""";
 
-	LONGS_EQUAL(true, result);
-	LONGS_EQUAL(PATH_MAPS_OK, error_code);
-	LONGS_EQUAL(1, xdebug_path_maps_get_rule_count(test_map));
+	result = test_map_from_file(map);
+	check_result(error_code, 1);
 
 	mapping = remote_to_local(test_map, "/var/www/");
-	LONGS_EQUAL(XDEBUG_PATH_MAP_TYPE_DIRECTORY, mapping->type);
-	STRCMP_EQUAL("/var/www/", mapping->remote_path);
-	STRCMP_EQUAL("/home/derick/projects/example.com/", mapping->local_path);
+
+	check_map(XDEBUG_PATH_MAP_TYPE_DIRECTORY, "/var/www/", "/home/derick/projects/example.com/");
 };
 
 TEST(path_maps_file, check_rules_with_prefix_1)
 {
-	xdebug_path_mapping *mapping = NULL;
-	result = xdebug_path_maps_parse_file(test_map, "files/with-prefix.map", &error_code, &error_message);
+	const char *map = R""""(
+remote_prefix: /var
+local_prefix: /home/derick/projects
+/www/ = /example.com/
+)"""";
 
-	LONGS_EQUAL(true, result);
-	LONGS_EQUAL(PATH_MAPS_OK, error_code);
-	LONGS_EQUAL(1, xdebug_path_maps_get_rule_count(test_map));
+	result = test_map_from_file(map);
+	check_result(error_code, 1);
 
 	mapping = remote_to_local(test_map, "/var/www/");
-	CHECK(mapping);
-	LONGS_EQUAL(XDEBUG_PATH_MAP_TYPE_DIRECTORY, mapping->type);
-	STRCMP_EQUAL("/var/www/", mapping->remote_path);
-	STRCMP_EQUAL("/home/derick/projects/example.com/", mapping->local_path);
+
+	check_map(XDEBUG_PATH_MAP_TYPE_DIRECTORY, "/var/www/", "/home/derick/projects/example.com/");
 };
