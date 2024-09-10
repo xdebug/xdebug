b38d105e60221d86e5bcb40fef2d0f7c6cff8c01
Derick Rethans <github@derickrethans.nl>
2024-09-10 12:45:23 +0100

    Implement mapping a remote line number to a local line number

--- src/lib/maps/maps_private.c
+++ src/lib/maps/maps_private.c
@@ -27,13 +27,49 @@ size_t xdebug_path_maps_get_rule_count(xdebug_path_maps *maps)
 	return maps->remote_to_local_map->size;
 }
 
-xdebug_path_mapping *remote_to_local(xdebug_path_maps *maps, const char *remote, size_t line)
+static size_t find_line_number_from_ranges(size_t remote_line, xdebug_path_map_range *ranges)
 {
-	void *result;
+	xdebug_path_map_range *ptr = ranges;
 
-	if (xdebug_hash_find(maps->remote_to_local_map, remote, strlen(remote), &result)) {
-		return (xdebug_path_mapping*) result;
+	do {
+		/* 1:1 match */
+		if (ptr->remote_begin == ptr->remote_end && ptr->local_begin == ptr->local_end && remote_line == ptr->remote_begin) {
+			return ptr->local_begin;
+		}
+		/* n:1 match */
+		if (ptr->local_begin == ptr->local_end && (remote_line >= ptr->remote_begin) && (remote_line <= ptr->remote_end)) {
+			return ptr->local_begin;
+		}
+		/* n:m match */
+		if ((remote_line >= ptr->remote_begin) && (remote_line <= ptr->remote_end)) {
+			return remote_line - ptr->remote_begin + ptr->local_begin;
+		}
+		ptr = ptr->next;
+	} while (ptr != NULL);
+
+	return -1;
+}
+
+/* Returns the remote type, and sets *local_path and *local_line if the type is not XDEBUG_PATH_MAP_TYPE_UNKNOWN */
+int remote_to_local(xdebug_path_maps *maps, const char *remote_path, size_t remote_line, xdebug_str **local_path, size_t *local_line)
+{
+	xdebug_path_mapping *result;
+
+	if (!xdebug_hash_find(maps->remote_to_local_map, remote_path, strlen(remote_path), (void**) &result)) {
+		return XDEBUG_PATH_MAP_TYPE_UNKNOWN;
 	}
 
-	return NULL;
+	*local_path = xdebug_str_copy(result->local_path);
+
+	switch (result->type) {
+		case XDEBUG_PATH_MAP_TYPE_DIRECTORY:
+		case XDEBUG_PATH_MAP_TYPE_FILE:
+			*local_line = remote_line;
+			break;
+		case XDEBUG_PATH_MAP_TYPE_LINES:
+			*local_line = find_line_number_from_ranges(remote_line, result->head_range_ptr);
+			break;
+	}
+
+	return result->type;
 }
