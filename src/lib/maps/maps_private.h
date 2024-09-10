8ed74ca79c1d18ee1ce644c4f065863af11bb740
Derick Rethans <github@derickrethans.nl>
2024-09-10 11:08:44 +0100

    Refactor to be able to store multiple line ranges per file

--- src/lib/maps/maps_private.h
+++ src/lib/maps/maps_private.h
@@ -25,16 +25,22 @@
 #define XDEBUG_PATH_MAP_TYPE_FILE      0x02
 #define XDEBUG_PATH_MAP_TYPE_LINES     0x03
 
-typedef struct xdebug_path_map_element {
-	xdebug_str *path;
-	int         begin;
-	int         end;
-} xdebug_path_map_element;
+typedef struct xdebug_path_map_range xdebug_path_map_range;
+
+struct xdebug_path_map_range {
+	int ref_count;
+	int remote_begin;
+	int remote_end;
+	int local_begin;
+	int local_end;
+	xdebug_path_map_range *next;
+};
 
 typedef struct xdebug_path_mapping {
-	int         type;
-	xdebug_path_map_element *remote;
-	xdebug_path_map_element *local;
+	int                      type;
+	xdebug_str              *remote_path;
+	xdebug_str              *local_path;
+	xdebug_path_map_range   *head_range_ptr;
 } xdebug_path_mapping;
 
 typedef struct xdebug_path_maps {
@@ -44,11 +50,13 @@ typedef struct xdebug_path_maps {
 /* Functions for testing, and not exported into Xdebug */
 size_t xdebug_path_maps_get_rule_count(xdebug_path_maps *maps);
 
-xdebug_path_mapping *remote_to_local(xdebug_path_maps *maps, const char *remote);
+xdebug_path_mapping *remote_to_local(xdebug_path_maps *maps, const char *remote, size_t line);
 
-xdebug_path_map_element* xdebug_path_map_element_ctor(void);
-void xdebug_path_map_element_dtor(xdebug_path_map_element *element);
+xdebug_path_map_range* xdebug_path_map_range_ctor(int remote_begin, int remote_end, int local_begin, int local_end);
+xdebug_path_map_range* xdebug_path_map_range_copy(xdebug_path_map_range *range);
+void xdebug_path_map_range_dtor(xdebug_path_map_range *range);
 
+xdebug_path_mapping *xdebug_path_mapping_ctor(void);
 void xdebug_path_mapping_dtor(void *mapping);
 
 #endif
