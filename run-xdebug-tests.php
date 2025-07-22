193a08757b91b85183d72ab4398c55b3d69582c0
Derick Rethans <github@derickrethans.nl>
2025-07-22 11:18:52 +0100

    Force variables_order setting for running tests, as some default distribututions don't have the 'E'

--- run-xdebug-tests.php
+++ run-xdebug-tests.php
@@ -311,6 +311,7 @@ function main(): void
         'zend.exception_string_param_max_len=15',
         'short_open_tag=0',
         'pcre.jit=0',
+        'variables_order=EGPCS',
     ];
 
     $no_file_cache = '-d opcache.file_cache= -d opcache.file_cache_only=0';
