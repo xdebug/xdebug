c02aaa8ad8f62a24f3ad8eef7481af567b119d7e
Derick Rethans <github@derickrethans.nl>
2025-12-04 15:28:20 +0000

    Tweak release instructions a little

--- .build.scripts/make-release.php
+++ .build.scripts/make-release.php
@@ -258,7 +258,7 @@ function createXdebugVersionPhp( $release_version )
 	global $xdebugOrgRepo;
 
 $xml = <<<ENDXML
-    public const NOT_SUPPORTED_BEFORE = '3.3';
+    public const NOT_SUPPORTED_BEFORE = '3.4';
     public const LATEST_VERSION = '{$release_version}';
     public const LATEST_WINDOWS_VERSION = '{$release_version}';
 
