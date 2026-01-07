4586c458d19ad36d400e610f3cc0edec3dfaeb22
Damjan Cvetko <damjan.cvetko@gmail.com>
2026-01-07 11:12:42 +0100

    Change error retrieval method in ctrl_socket.c
    Replaced WSAGetLastError with GetLastError for error handling.

--- src/base/ctrl_socket.c
+++ src/base/ctrl_socket.c
@@ -495,7 +495,7 @@ void xdebug_control_socket_setup(void)
 	);
 
 	if (XG_BASE(control_socket_h) == INVALID_HANDLE_VALUE) {
-		errno = WSAGetLastError();
+		errno = GetLastError();
 		xdebug_log_ex(XLOG_CHAN_CONFIG, XLOG_WARN, "CTRL-SOCKET", "Can't create control Named Pipe (0x%x)", errno);
 		xdfree(XG_BASE(control_socket_path));
 		XG_BASE(control_socket_path) = NULL;
